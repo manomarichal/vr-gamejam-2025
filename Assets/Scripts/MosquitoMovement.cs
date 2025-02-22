927b42c91dde4faee4017f8aee68e89dde57cfb8
Basil Rommens <basil.rommens@uantwerpen.be>
2025-02-22 20:39:45 +0100

    don't poop your pants with your socks on

--- Assets/Scripts/MosquitoMovement.cs
+++ Assets/Scripts/MosquitoMovement.cs
@@ -7,18 +7,34 @@ public class MosquitoMovement : MonoBehaviour
 
     private Vector3[] controlPoints = new Vector3[4]; // Bezier curve control points
     private float t = 0; // Bezier curve parameter (0 to 1)
+    private Vector3 lastPosition; // Store last position for rotation calculation
 
     void Start()
     {
         GenerateNewBezierCurve();
+        lastPosition = transform.position;
     }
 
     void Update()
     {
+        
+    }
+
+    void FixedUpdate() {
         if (t < 1)
         {
             t += speed * Time.deltaTime; // Increase t based on speed
-            transform.position = BezierCurve(t, controlPoints[0], controlPoints[1], controlPoints[2], controlPoints[3]);
+            Vector3 newPosition = BezierCurve(t, controlPoints[0], controlPoints[1], controlPoints[2], controlPoints[3]);
+
+            // // Calculate direction vector
+            // Vector3 direction = (newPosition - lastPosition).normalized;
+            // if (direction != Vector3.zero) // Ensure we don't rotate to NaN
+            // {
+            //     transform.rotation = Quaternion.LookRotation(direction);
+            // }
+
+            transform.position = newPosition;
+            lastPosition = newPosition; // Update last position
         }
         else
         {
@@ -31,54 +47,35 @@ public class MosquitoMovement : MonoBehaviour
     {
         Vector3 centerPosition = Camera.main.transform.position;
 
-        // ensure that the distance between the two positions is large enough
         if (Vector3.Distance(position, otherPosition) < 2.0f)
-        {
             return false;
-        }
-        // ensure that the position is outside of the radius of the root position
+
         if (Vector3.Distance(position, centerPosition) < 1.0f)
-        {
             return false;
-        }
-        // ensure that the position is within the quarter sphere in front of the user
+
         return (position.magnitude <= SPHERE_RADIUS);
     }
 
     void GenerateNewBezierCurve()
     {
         Vector3 startPos = transform.position;
-        // ensure that this point is within the quarter sphere in front of the player
         Vector3 endPos = GenerateRandomPosition();
-        while (!checkValidPosition(startPos, endPos))
-        {
-            endPos = GenerateRandomPosition();
-        }
-        
-        
-        // Control points create a smooth curve
+
         controlPoints[0] = startPos;
-        controlPoints[1] = startPos + Random.insideUnitSphere * SPHERE_RADIUS * 0.5f; // First control point
-        controlPoints[2] = endPos + Random.insideUnitSphere * SPHERE_RADIUS * 0.5f; // Second control point
+        controlPoints[1] = startPos + Random.insideUnitSphere * SPHERE_RADIUS * 0.5f;
+        controlPoints[2] = endPos + Random.insideUnitSphere * SPHERE_RADIUS * 0.5f;
         controlPoints[3] = endPos;
     }
 
     Vector3 GenerateRandomPosition()
     {
         Vector3 center = Camera.main.transform.position;
-        // ensure that the position is within the quarter sphere in front of the user
-        while (true)
-        {
+        // use angles instead of ranges, it will be better in equal distribution of mosquito positions
             float x = Random.Range(-SPHERE_RADIUS, SPHERE_RADIUS);
             float y = Random.Range(-SPHERE_RADIUS, 0);
             float z = Random.Range(0, SPHERE_RADIUS);
             Vector3 position = new Vector3(x, y, z) + center;
-
-            if (position.magnitude <= SPHERE_RADIUS)
-            {
-                return position;
-            }
-        }
+        return position;
     }
 
     Vector3 BezierCurve(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
