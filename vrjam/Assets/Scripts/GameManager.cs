c38dc2eaa92775473f9c64da8d21c7834bb7f7c8
Arno Deceuninck <arnodece@gmail.com>
2025-02-23 12:27:35 +0100

    Update clock time

--- vrjam/Assets/Scripts/GameManager.cs
+++ vrjam/Assets/Scripts/GameManager.cs
@@ -5,7 +5,7 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    public int maxTime = 180;
+    public int maxTime = 20; // Max game time in seconds
     public int maxMosquitos = 3;
     public AudioClip[] audioClips; // This is the exported variable
 
@@ -19,6 +19,13 @@ public class GameManager : MonoBehaviour
     private int _tutorialPhase = 0;
 
     private static int killCount = 0; // Static variable to keep track of the kill count
+    private static float gameTime = 0; // Current game time in seconds
+
+    private static int startHour = 22;
+    // private static int startMinute = 0;
+    private static int endHour = 7;
+    // private static int endMinute = 0;
+    private static int totalGameMinutes = 9 * 60; // 9 hours
 
     // Start is called before the first frame update
     void Start()
@@ -26,21 +33,65 @@ public class GameManager : MonoBehaviour
         _tutorialMosquito = SpawnMosquito();
         _tutorialMosquito.GetComponent<MosquitoMovement>().tutorialMode = true;
         _tutorialPhase = 1;
+        gameTime = 0;
+        // clockText.text = startHour.ToString("00") + ":" + startMinute.ToString("00");
+        clockText.text = "21:00";
     }
 
     // Update is called once per frame
     void Update()
     {
+        gameTime += (float)Time.deltaTime;
+
         if (_tutorialPhase == 1 && _tutorialMosquito == null)
         {
             _tutorialPhase = 2;
+            gameTime = 0;
         }
 
         if (_tutorialPhase == 2){
+            // Tutorial just finished, start normal gameplay
             SpawnMosquito();
             StartCoroutine(SpawnMosquitoWithDelay());
             _tutorialPhase = 3;
+            gameTime = 0;
         }
+
+        if (_tutorialPhase == 3) {
+            // No tutorial anymore
+            UpdateClock();
+
+            if (gameTime >= maxTime) {
+                PlayClockSound();
+                SetEndView(); 
+                _tutorialPhase = 4;
+            }
+        }
+    }
+
+    void PlayClockSound() {
+        // TODO: Play alarm beep sound (followed by our Mosquito song)
+    }
+
+    void SetEndView () {
+        // TODO: Show "Clap to restart game"
+        // Keep alarm view flickering on/off
+        // Show credit scene
+    }
+
+    void UpdateClock() {
+        float gameProgress = gameTime / maxTime;
+        // update currentHour based on gameProgress, startHour and endHour. However, note that startHour is e.g. 22 and endHour is 7, so you need to handle the case where the game goes past midnight.
+        int endHourAdjusted = endHour + (startHour > endHour ? 24 : 0);
+
+        int currentHour = startHour + (int)(gameProgress * (endHourAdjusted - startHour));
+        int currentMinute = 0;
+
+        if (currentHour > 23) {
+            currentHour -= 24;
+        }
+
+        clockText.text = currentHour.ToString("00") + ":" + currentMinute.ToString("00");
     }
 
     void FixedUpdate()
@@ -52,7 +103,7 @@ public class GameManager : MonoBehaviour
     {
         while (true)
         {
-            yield return new WaitForSeconds(Random.Range(7, 15)); // Random interval'
+            yield return new WaitForSeconds(Random.Range(4, 10)); // Random interval'
             if (_aliveMosquitos < maxMosquitos) {
                 SpawnMosquito();
             }
